d1bb125d40181554b8a9893a41ad75195c1b25fd
jainv4156 <jainv4156@gmail.com>
2024-04-20 20:14:47 +0530

    remving vercel deployment

--- onlineauction/onlineauction/settings.py
+++ onlineauction/onlineauction/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-jpait&e812+)sm*^g!5x+o0u2zemr%g7u1yq5+wkl8f1^xz$3*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','localhost','127.0.0.1']
-
+ALLOWED_HOSTS = []
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
@@ -128,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build','static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media') 
 MEDIA_URL ='/media/' 
