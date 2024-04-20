7442f00167f3ef22eb6cb3475c1fcd4c62a0f9c9
jainv4156 <jainv4156@gmail.com>
2024-04-20 19:58:48 +0530

    deploying

--- onlineauction/onlineauction/settings.py
+++ onlineauction/onlineauction/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-jpait&e812+)sm*^g!5x+o0u2zemr%g7u1yq5+wkl8f1^xz$3*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','localhost','127.0.0.1']
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
@@ -127,9 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build','static')
+
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media') 
 MEDIA_URL ='/media/' 
 
